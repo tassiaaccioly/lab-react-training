7c7e71e09a18f56b007c1598913bf834cfd07066
Tassia Accioly <tassia.accioly@gmail.com>
2020-11-12 20:26:23 -0300

    LikeButton bonus done

--- src/likebutton/LikeButton.js
+++ src/likebutton/LikeButton.js
@@ -1,17 +1,27 @@
 import React, { useState } from 'react';
 import './LikeButton.css';
 
-const colors = ['purple', 'blue', 'green', 'yellow', 'orange', 'red'];
-
 const LikeButton = (props) => {
-  let [value, setValue] = useState(0);
+  let colorsArr = ['purple', 'blue', 'green', 'yellow', 'orange', 'red'];
+  let idx = 0;
+  let colors = colorsArr[idx];
+  const [value, setValue] = useState(0);
+  let [color, setColor] = useState(colors);
 
   const handleClick = () => {
     setValue(value + 1);
+    idx = Math.floor(Math.random() * 6);
+    colors = colorsArr[idx];
+    color = colors;
+    setColor(color);
   };
 
   return (
-    <button onClick={handleClick} className="likebutton">
+    <button
+      onClick={handleClick}
+      className="likebutton"
+      style={{ backgroundColor: color }}
+    >
       {value} Like{value === 1 ? '' : 's'}
     </button>
   );
